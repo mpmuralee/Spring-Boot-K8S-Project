3aaa72d275e4e7db6913b26f9187cccf250df495
muralidhar <muralee.cmc@gmail.com>
2022-12-15 11:22:47 +0100

    Adding Logger and Updating Readme

--- src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
+++ src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
@@ -1,5 +1,7 @@
 package com.javatechie.k8s;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SpringbootK8sDemoApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SpringbootK8sDemoApplication.class);
+	   
+    
 
 	@GetMapping("/message")
 	public String displayMessage(){
+		 logger.info("this is a info message");
+	     logger.warn("this is a warn message");
+	     logger.error("this is a error message");
 		return "Push Third time Congratulation you successfully deployed your application to kubernetes !!";
 	}
 
